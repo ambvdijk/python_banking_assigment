1cba8ec24847334781908065cbe40fa373cc60c0
Mathijs van Dijk <ambvdijk@gmail.com>
2023-01-16 16:20:07 +0100

    Cleanup

--- src/program.py
+++ src/program.py
@@ -1,6 +1,7 @@
 from .authentication_menu import AuthenticationMenu
 from .account_service import AccountService
 
+# TODO: Add (file,database) store to actually store the accounts
 class Program:
     def run(self) -> None:
         AuthenticationMenu(AccountService([])).run()
